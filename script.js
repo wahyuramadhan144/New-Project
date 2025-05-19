673079478d74017d81fa4645499dae69bffe3f3e
wahyuramadhan <wahyuganss33@gmail.com>
2025-05-19 19:41:11 +0700

    Update layout notif

--- script.js
+++ script.js
@@ -169,21 +169,13 @@ function startCountdownAndCapture() {
 }
 
 function showToast(message) {
-  const toast = document.createElement('div');
-  toast.textContent = message;
-  toast.style.position = 'fixed';
-  toast.style.bottom = '80px';
-  toast.style.left = '50%';
-  toast.style.transform = 'translateX(-50%)';
-  toast.style.color = 'white';
-  toast.style.padding = '25px 10px';
-  toast.style.borderRadius = '5px';
-  toast.style.zIndex = 9999;
-  toast.style.fontSize = '1rem';
-  toast.style.fontFamily = 'Arial';
-  document.body.appendChild(toast);
-  setTimeout(() => {
-    document.body.removeChild(toast);
+  const notif = document.getElementById('notificationContainer');
+  notif.textContent = message;
+  notif.style.opacity = '1';
+
+  clearTimeout(showToast.timeoutId);
+  showToast.timeoutId = setTimeout(() => {
+    notif.style.opacity = '0';
   }, 2000);
 }
 
